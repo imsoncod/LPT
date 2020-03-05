58187d0ce9bc2c2d475dc5a5d78a1934dc7c0c1a
mc_memory@naver.com <mc_memory@naver.com>
2020-03-05 15:24:12 +0900

    LPT

--- src/model/TestDTO.java
+++ src/model/TestDTO.java
@@ -1,5 +1,34 @@
 package model;
 
+//Last Update : 2020-03-05
 public class TestDTO {
-
+	private int num;
+	private String nickname;
+	private String result;
+	
+	public int getNum() {
+		return num;
+	}
+	
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+	
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
 }
